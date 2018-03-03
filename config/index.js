2f95e99dd6387eaa8c16772d8b7344d534a2ae9f
chhpt <cwuyiqing@gmail.com>
2018-03-03 16:50:34 +0800

    修复配置问题

--- config/index.js
+++ config/index.js
@@ -3,6 +3,6 @@ const prod = require('./prod.js');
 
 const env = process.env.NODE_ENV;
 
-const config = env === 'prod' ? prod : dev;
+const config = env === 'dev' ? dev : prod;
 
 module.exports = config;
